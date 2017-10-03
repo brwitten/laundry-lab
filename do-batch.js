21c15a369a4aca5d09d4bda39537b60afb7a7bfd
brwitten <bethrwitten@gmail.com>
2017-10-02 21:42:48 -0700

    attempt at do-batch

--- do-batch.js
+++ do-batch.js
@@ -4,15 +4,31 @@ var batch_input = [
   ["rEd tuX/edo", "red* soCks"]
 ];
 
-/* TODO: copy `clean` function here */
-/* TODO: copy `mend` function here */
-/* TODO: copy `iron` function here */
-
-
-/* YOUR CODE HERE */
-function doBatch(clothes){
-    // your code here
+function clean(element){
+    clean_clothing_item = clothing_item.replace("*","");
+    return mended_clothing_item;
 }
+
+function mend(element){
+    mended_clothing_item = clothing_item.replace("/","");
+    return mended_clothing_item;
+}
+
+function iron(element){
+    var lowerCase = clothing_item.toLowerCase();
+    return lowerCase;
+}
+
+
+// function doBatch(clothes){
+//}
+
+batch_input.forEach(function(element){
+  clean(element);
+  mend(element);
+  iron(element);
+})
+
 
 
 /* PRINT RESULT */
